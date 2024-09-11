a7eeaf71239364b0164f8f67e91c9299436b778d
IshwarGupta1 <guptaishwar1998@gmail.com>
2024-09-12 00:26:06 +0530

    dotnet crud start

--- projects/scrumpoker/scrumpoker/Models/Card.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ScrumPoker.Models
-{
-    public class Card
-    {
-        public int CardId { get; set; } // primary key
-        public int CardValue { get; set; } //fibonacci value
-    }
-}
--- projects/scrumpoker/scrumpoker/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ScrumPoker.Models
-{
-    public class User
-    {
-        public int UserId { get; set; } // primary key
-        public string UserName { get; set; } // name
-        public string Role { get; set; } // Dev or QA
-    }
-}

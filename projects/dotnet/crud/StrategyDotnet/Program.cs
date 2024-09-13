ee123e978ec4eb479e69be6a7dd7e8b6c5fd2f02
IshwarGupta1 <guptaishwar1998@gmail.com>
2024-09-13 22:57:53 +0530

    strategy

---
+++ projects/dotnet/crud/StrategyDotnet/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StrategyDotnet;
+
+Console.WriteLine("Hello, World!");
+Context context1 = new Context(new ConcreteStrategyA());
+Context context2 = new Context(new ConcreteStrategyB());
+context1.ExecuteStrategy();
+context2.ExecuteStrategy();

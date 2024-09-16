63014cb1beaf0f88f8526484c92f0820835f43dd
IshwarGupta1 <guptaishwar1998@gmail.com>
2024-09-16 22:26:37 +0530

    potd

---
+++ projects/dotnet/crud/encapsulation/BankAccountManagement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encapsulation
+{
+    public class BankAccountManagement
+    {
+        private Decimal _balance; 
+        private Decimal _minimumBalance;
+        public BankAccountManagement(Decimal balance, decimal minimumBalance)
+        {
+            _balance = balance;
+            _minimumBalance = minimumBalance;
+        }
+        public void withdraw(Decimal amount)
+        {
+            if (amount > _balance)
+                throw new Exception("not enough balance");
+            var remainingBalance = _balance - amount;
+            if (remainingBalance < _minimumBalance)
+            {
+                throw new Exception("remaining Balance is going less than minimumBalance");
+            }
+            Console.WriteLine($"Amount withdrawn {amount}. Remaining Balance is {remainingBalance}");
+        }
+    }
+}

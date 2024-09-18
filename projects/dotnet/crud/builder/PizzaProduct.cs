a095cbe3ef13acc0f5f22d969174c21db14f7a6e
IshwarGupta1 <guptaishwar1998@gmail.com>
2024-09-18 07:49:47 +0530

    POTD 17th sept and builder pattern

---
+++ projects/dotnet/crud/builder/PizzaProduct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace builder
+{
+    public class PizzaProduct
+    {
+        private string crust;
+        private string cheeze;
+        private string topping;
+        private string pepporoni;
+        public PizzaProduct(PizzaBuilder builder)
+        {
+            crust = builder.Crust;
+            cheeze = builder.Cheeze;
+            topping = builder.Topping;
+            pepporoni = builder.Pepporoni;
+        }
+        public override string ToString()
+        {
+            return $"Pizza with {crust} crust, {cheeze} cheeze, {topping} topping, and {pepporoni} pepporoni.";
+        }
+    }
+}

877b82a52638c3313a0b24e59a7ef7d706d16b2d
IshwarGupta1 <guptaishwar1998@gmail.com>
2024-09-30 23:35:58 +0530

    strategy code

---
+++ projects/dotnet/crud/strategy/Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strategy
+{
+    public class Context
+    {
+        public IPaymentStrategy strategy;
+        public Context(IPaymentStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public void payment(int amount)
+        {
+            strategy.pay(amount);
+        }
+    }
+}

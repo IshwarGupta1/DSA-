850e7ace808b436648e634ae249868aa2e183a5e
IshwarGupta1 <guptaishwar1998@gmail.com>
2024-12-17 20:48:51 +0530

    singleton pattern

---
+++ DesignPatterns/Singleton/Singleton/HashInstance.cs
@@ -0,0 +1,15 @@
+﻿namespace Singleton
+{
+    public class HashInstance
+    {
+        private static HashInstance _hashInstance;
+        private HashInstance() { 
+        }
+        public static HashInstance getInstance()
+        {
+            if( _hashInstance == null )
+                _hashInstance = new HashInstance();
+            return _hashInstance;
+        }
+    }
+}
---
+++ DesignPatterns/Singleton/Singleton/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Singleton;
+
+Console.WriteLine("Hello, World!");
+var hash1 = HashInstance.getInstance();
+var hash2 = HashInstance.getInstance();
+Console.WriteLine(hash1==hash2);

cece2c76cdcfbf2ad518cc6fcbadc5ec4963ebf1
IshwarGupta1 <guptaishwar1998@gmail.com>
2025-02-08 00:01:07 +0530

    changes

---
+++ devpractice/machinecoding/atm/ATM/Models/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM.Models
+{
+    public class Card
+    {
+        public string CardNo { get; set; }
+        public string CardPIN { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ devpractice/machinecoding/atm/ATM/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace ATM.Models
+{
+    public class User
+    {
+        public string UserId { get; set; }
+        public string CardNumber { get; set; }
+        public string Pin { get; set; }
+        public Account Account { get; set; }
+
+        public User(string userId, string cardNumber, string pin, Account account)
+        {
+            UserId = userId;
+            CardNumber = cardNumber;
+            Pin = pin;
+            Account = account;
+        }
+    }
+
+}

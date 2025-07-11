39b8ba3303238737b085a5cd430572b41f3e5b5c
IshwarGupta1 <guptaishwar1998@gmail.com>
2025-07-11 22:33:51 +0530

    LC 525

---
+++ LeetCodeDSA/LC525.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        vector<int> prefixSum(nums.size());
+        unordered_map<int, int> mp;
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += (nums[i] == 0) ? -1 : 1;
+            prefixSum[i] = sum;QQQ
+        }
+
+        int maxLen = 0;
+        mp[0] = -1;
+
+        for (int i = 0; i < prefixSum.size(); i++) {
+            int s = prefixSum[i];
+            if (mp.count(s)) {
+                maxLen = max(maxLen, i - mp[s]);
+            } else {
+                mp[s] = i;
+            }
+        }
+        return maxLen;
+    }
+};
